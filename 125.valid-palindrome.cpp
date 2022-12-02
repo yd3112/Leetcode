bafad3ea4d8616659a468a05ee914bb8bbde8a2b
yd <pokar.yashusmile@gmail.com>
2022-12-02 11:07:08 +0530

    uber problem solved

---
+++ 125.valid-palindrome.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=125 lang=cpp
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j= s.size() - 1;
+        while( i < j){
+            while(i < j && !isalnum(s[i]))
+                i++;
+            while(i < j && !isalnum(s[j]))
+                j--;
+            if(tolower(s[i]) != tolower(s[j]))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
+// @lc code=end
+
